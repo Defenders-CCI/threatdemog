bcaa7f933a3bb8f7f9a9b232175c3955028cb578
Jacob Malcom <jmalcom@defenders.org>
2016-06-04 11:29:17 -0400

    Update to BSD 2-cluase license, reorded models in vignette.

---
+++ inst/CITATION.R
@@ -0,0 +1,20 @@
+citHeader("To cite this analysis, please cite the paper:")
+
+citEntry(entry = "article",
+         author = personList(
+                    as.person("Jacob W. Malcom"),
+                    as.person("Whitney M. Webber"),
+                    as.person("Ya-Wei Li")
+         ),
+         title = "A simple, sufficient, and consistent method to score the status of threats and demography of imperiled species",
+         publisher = "PeerJ Preprints",
+         year = "2016",
+         url = "https://peerj.com/preprints/1860/",
+         textVersion = paste("Malcom, J.W., W.M. Webber, and Y-W. Li.",
+                             "2016.",
+                             "A simple, sufficient, and consistent method to score
+                             the status of threats and demography of imperiled
+                             species.",
+                             "PeerJ Preprints, 4:e1860v1
+                             https://doi.org/10.7287/peerj.preprints.1860v1")
+)
